9a346f1959cc8d3b1f8a3a14a73d878ffe78b145
sfalabake <solomonfalabake@yahoo.com>
2026-05-20 14:50:03 +0100

    programming with modules

---
+++ week3/scripts/course.mjs
@@ -0,0 +1,41 @@
+// scripts/course.mjs
+
+const byuiCourse = {
+    code: "WDD231",
+    name: "Web Frontend Development I",
+    sections: [
+        {
+            sectionNumber: 1,
+            enrolled: 88,
+            instructor: "Brother Bingham",
+        },
+        {
+            sectionNumber: 2,
+            enrolled: 81,
+            instructor: "Sister Shultz",
+        },
+        {
+            sectionNumber: 3,
+            enrolled: 95,
+            instructor: "Sister Smith",
+        },
+    ],
+
+    changeEnrollment: function (sectionNumber, add = true) {
+        const sectionIndex = this.sections.findIndex(
+            (section) => section.sectionNumber == sectionNumber
+        );
+
+        if (sectionIndex >= 0) {
+            if (add) {
+                this.sections[sectionIndex].enrolled++;
+            } else {
+                this.sections[sectionIndex].enrolled--;
+            }
+
+            // ❌ removed renderSections call (now handled in modules.mjs)
+        }
+    },
+};
+
+export default byuiCourse;
